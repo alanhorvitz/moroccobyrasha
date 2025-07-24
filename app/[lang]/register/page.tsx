57f18aa69df0dfa5cd25ef9edaa1bfa0c798db81
ZakariaAit51 <zakariamadrid762@gmail.com>
2025-07-24 13:26:51 +0100

    Remove logo and title from login and register pages for a cleaner design

--- app/[lang]/register/page.tsx
+++ app/[lang]/register/page.tsx
@@ -12,10 +12,6 @@ export default function RegisterPage() {
       </div>
       <div className="relative hidden h-full flex-col bg-muted p-10 text-white lg:flex dark:border-r">
         <div className="absolute inset-0 bg-primary" />
-        <div className="relative z-20 flex items-center text-lg font-medium">
-          <img src="/logo.svg" alt="" className="mr-2 h-6 w-6" />
-          Morocco By Rasha
-        </div>
         <div className="relative z-20 mt-auto">
           <blockquote className="space-y-2">
             <p className="text-lg">

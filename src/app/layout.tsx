a68d89e912518e229edbd0134ee4210770b15cf8
ZakariaAit51 <zakariamadrid762@gmail.com>
2025-07-31 17:39:54 +0100

    Update ThemeProvider in RootLayout to set default theme to 'light'

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -39,7 +39,7 @@ export default function RootLayout({
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </head>
       <body className={inter.className}>
-        <ThemeProvider attribute="class">
+        <ThemeProvider attribute="class" defaultTheme="light">
           <ReactQueryProvider>
             <LanguageProvider>
               <AuthProvider>
